47a0680c06a771f1a433b205e3dd1442b7175484
Rookie <alexlol2334@gmail.com>
2026-09-13 17:40:33 +0100

    Modified XML Command Usage

--- Scripts/5_Mission/CFXMLTest.c
+++ Scripts/5_Mission/CFXMLTest.c
@@ -5,37 +5,16 @@ class CFXMLTest
         Print("[E205][CFXML] ========================================");
         Print("[E205][CFXML] CF XML Test Starting");
 
-        CF_XML xml = new CF_XML();
+        CF_XML_Reader reader = CF_XML.Open("E205Lib/Config/Settings.xml");
 
-        Print("[E205][CFXML] CF_XML object created");
-
-        if (!xml.LoadFile("E205Lib/Config/Settings.xml"))
+        if (!reader)
         {
-            Print("[E205][CFXML] ERROR: Failed to load Settings.xml");
+            Print("[E205][CFXML] ERROR: Failed to open Settings.xml");
             return;
         }
 
-        Print("[E205][CFXML] Settings.xml loaded successfully");
-
-        CF_XMLNode root = xml.GetRoot();
-
-        if (!root)
-        {
-            Print("[E205][CFXML] ERROR: XML root is null");
-            return;
-        }
-
-        Print("[E205][CFXML] Root node obtained");
-
-        CF_XMLNode messageNode = root.Find("TestMessage");
-
-        if (!messageNode)
-        {
-            Print("[E205][CFXML] ERROR: TestMessage node not found");
-            return;
-        }
-
-        Print("[E205][CFXML] TestMessage: " + messageNode.GetValue());
+        Print("[E205][CFXML] Settings.xml opened successfully");
+        Print("[E205][CFXML] Reader returned successfully");
 
         Print("[E205][CFXML] ========================================");
         Print("[E205][CFXML] CF XML Test Complete");
